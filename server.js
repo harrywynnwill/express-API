a233b7e812a91a7a0dc29310ad36934b2969f37b
Harry Wynn-Williams <harrywynnwilliams@googlemail.com>
2016-09-29 16:56:20 +0100

    experimenting

--- server.js
+++ server.js
@@ -2,9 +2,25 @@ var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 8080;
+
+
 var github = require('./app/models/github');
+
+
 var router = express.Router();
+var options = {
+  url: "https://api.github.com/users/harrywynnwill",
+  headers: {
+    'User-Agent': 'request'
+  }
+};
+
+
+
 var request = require('request');
+
+
+
 
 router.use(function(req, res, next) {
     // do logging
@@ -12,10 +28,22 @@ router.use(function(req, res, next) {
     next(); // make sure we go to the next routes and don't stop here
 });
 
+function getGitHub() {
+
+  request(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      return body;
+      console.log(body)
+    }
+  })
+};
+
 
 router.get('/', function(req, res) {
 
-   res.json({ message: 'hooray! welcome to our api!' });
+
+
+
 
 });
 
@@ -24,3 +52,9 @@ app.use('/api', router);
 
 app.listen(port);
 console.log('Magic happens on port ' + port);
+
+
+
+function _handleResponseFromApi (response) {
+  return response.data;
+}
